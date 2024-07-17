9e0d8c06636ff7d0fcacbd51a8adcc1d26990974
Ecank <113890228+Ecank-Officials@users.noreply.github.com>
2024-07-17 17:14:55 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use bevy::prelude::*;
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct Player {
     name: String,
 }
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct Position {
     x: f32,
     y: f32,
 }
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct Velocity {
     x: f32,
     y: f32,
@@ -16,11 +16,11 @@ struct Velocity {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, init)
-        .add_systems(Update, player_movement)
+        .add_systems(Startup, (player_spawn, enemy_spawn, obstacles_spawn))
+        .add_systems(Update, (player_movement, print_postion))
         .run();
 }
-fn init(mut commands: Commands) {
+fn player_spawn(mut commands: Commands) {
     commands.spawn((
         Player {
             name: "player".to_string(),
@@ -29,21 +29,44 @@ fn init(mut commands: Commands) {
         Velocity { x: 0.0, y: 0.0 },
     ));
 }
+fn enemy_spawn(mut commands: Commands) {
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 0.0, y: 0.0 }));
+}
+fn obstacles_spawn(mut commands: Commands) {
+    commands.spawn(Position { x: 0.0, y: 0.0 });
+}
 fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&Player, &mut Position, &mut Velocity)>,
 ) {
     for (_player, mut position, mut velocity) in query.iter_mut() {
         let mut direction_x = 0.0;
-
+        let mut direction_y = 0.0;
+        let g = 0.9;
         if keyboard_input.just_pressed(KeyCode::KeyA) {
-            direction_x -= 1.0;
+            direction_x = -1.0;
         }
         if keyboard_input.just_pressed(KeyCode::KeyD) {
-            direction_x += 1.0;
+            direction_x = 1.0;
+        }
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            direction_y = 1.0;
         }
         velocity.x = direction_x;
-
+        velocity.y = direction_y;
         position.x += velocity.x * 1.0;
+        position.y += velocity.y * 1000.0;
+        if velocity.y == 0.0 && position.y > 0.0 {
+            position.y -= g;
+        }
+    }
+}
+
+fn print_postion(query: Query<(Entity, &Player, &Position, &Velocity)>) {
+    for (entity, player, position, _velocity) in query.iter() {
+        info!(
+            "Entity{:?}{:?} is at position {:?},",
+            entity, player.name, position
+        );
     }
 }
